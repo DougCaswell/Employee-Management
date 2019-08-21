ff955ddbdec6294be87e92341a25d61a7d890823
DougCaswell <dougcaswell@gmail.com>
2019-08-21 12:14:18 -0600

    added styling with SASS

--- src/Components/AddEmployee.js
+++ src/Components/AddEmployee.js
@@ -25,20 +25,31 @@ class AddEmployee extends Component {
 
     render() {
         return (
-            <div>
+            <div className='AddEmployeeWrapper'>
                 <h2>Add new employee to system</h2>
                 <form onSubmit={() => this.add()}>
-                    <div className="left">
-                        <label htmlFor="firstName">First Name</label>
-                        <input type="text" onChange={e => this.setState({ firstName: e.target.value })} name="firstName" id="firstName" required />
-                        <label htmlFor="lastName">Last Name</label>
-                        <input type="text" onChange={e => this.setState({ lastName: e.target.value })} name="lastName" id="lastName" required />
-                    </div>
-                    <div className="right">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" onChange={e => this.setState({ email: e.target.value })} name="email" id="email" required />
-                        <label htmlFor="phone">Phone</label>
-                        <input type="text" onChange={e => this.setState({ phone: e.target.value })} name="phone" id="phone" required />
+                    <div className='FormSections'>
+                        <div className="left">
+                            <div className='top'>
+                                <label htmlFor="firstName">First Name</label>
+                                <input type="text" onChange={e => this.setState({ firstName: e.target.value })} name="firstName" id="firstName" required />
+                            </div>
+                            <div className='bottom'>
+                                <label htmlFor="lastName">Last Name</label>
+                                <input type="text" onChange={e => this.setState({ lastName: e.target.value })} name="lastName" id="lastName" required />
+                            </div>
+                        </div>
+                        <div className="right">
+                            <div className='top'>
+                                <label htmlFor="email">Email</label>
+                                <input type="email" onChange={e => this.setState({ email: e.target.value })} name="email" id="email" required />
+                            </div>
+                            <div className='bottom'>
+                                <label htmlFor="phone">Phone</label>
+                                <input type="text" onChange={e => this.setState({ phone: e.target.value })} name="phone" id="phone" required />
+                            </div>
+                        </div>
+
                     </div>
                     <div className="submit">
                         <input type="submit" value="Hired!" />
